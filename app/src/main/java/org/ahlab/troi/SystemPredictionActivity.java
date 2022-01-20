70699713f22965710e32f4ac45e3dc43d57386c8
Vipula Dissanayake <vipula.11@cse.mrt.ac.lk>
2022-01-21 10:29:49 +1300

    self report view

---
+++ app/src/main/java/org/ahlab/troi/SystemPredictionActivity.java
@@ -0,0 +1,14 @@
+package org.ahlab.troi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SystemPredictionActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_system_prediction);
+	}
+}
