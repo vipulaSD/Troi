a1429dffdf68dd154c137e204a151f81c35ef811
Vipula Dissanayake <vipula.11@cse.mrt.ac.lk>
2022-01-24 02:19:17 +1300

    remap av values from [-1,0,1] to [0,1,2]

--- app/src/main/java/org/ahlab/troi/AVResultFragment.java
+++ app/src/main/java/org/ahlab/troi/AVResultFragment.java
@@ -28,17 +28,17 @@ public class AVResultFragment extends Fragment {
 		binding = FragmentAVResultBinding.inflate(inflater, container, false);
 		if (getArguments() != null) {
 			int arousal = getArguments().getInt(getString(R.string.key_pred_arousal));
-			if (arousal > 0) {
+			if (arousal > 1) {
 				binding.txtArousal.setText(getString(R.string.as_high_arousal));
-			} else if (arousal < 0) {
+			} else if (arousal < 1) {
 				binding.txtArousal.setText(getString(R.string.as_low_arousal));
 			} else {
 				binding.txtArousal.setText(getString(R.string.as_neutral));
 			}
 			int valence = getArguments().getInt(getString(R.string.key_pred_valence));
-			if (valence > 0) {
+			if (valence > 1) {
 				binding.txtValence.setText(getString(R.string.as_positive_valence));
-			} else if (valence < 0) {
+			} else if (valence < 1) {
 				binding.txtValence.setText(getString(R.string.as_negative_valence));
 			} else {
 				binding.txtValence.setText(getString(R.string.as_neutral));
